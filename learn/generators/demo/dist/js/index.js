961c3ef9c62cca8504d0499e3449cbda266b41cd
guojin <jerrychane@126.com>
2020-04-06 22:33:45 +0800

    2020.04.06 MON 15WEEK Beijing yeoman 学习

---
+++ learn/generators/demo/dist/js/index.js
@@ -0,0 +1,1 @@
+console.log("hello,imooc！"),console.log("hello,imooc！！"),console.log("hello,imooc!!!!"),console.log("hello,imooc!!!!"),console.log("hello,imooc!!!!!"),console.log("hello,imooc!!!!!");

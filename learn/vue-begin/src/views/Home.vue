1167ff7e2308ae98bf134b22892163d2f470982d
guojin <jerrychane@126.com>
2020-04-15 22:46:38 +0800

    2020.04.15 WED 15WEEK Beijing

--- learn/vue-begin/src/views/Home.vue
+++ learn/vue-begin/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    {{msg1}}
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -11,6 +12,11 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      msg1: 'hello imooc'
+    }
+  },
   components: {
     HelloWorld
   }

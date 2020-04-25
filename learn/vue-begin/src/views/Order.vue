9390a7f27f972ad4d034a9336bd4ef52902d1ec1
guojin <jerrychane@126.com>
2020-04-25 22:03:38 +0800

    2020.04.25 SAT 17WEEK Beijing Vue Learn

--- learn/vue-begin/src/views/Order.vue
+++ learn/vue-begin/src/views/Order.vue
@@ -3,10 +3,12 @@
     <div class="left">
       左侧是下单
       <ul>
-        <li
-          v-for="(item,index) in lists"
-          :key="'order'+index"
-        >{{item.name}}  - {{item.price}}</li>
+        <li v-for="(item,index) in lists" :key="'order'+index">
+          <span>{{item.name}} - {{item.price}}</span>
+          <button type="button" @click="minus(item,index)">-</button>
+          <span>{{typeof item.num === "undefined" ? 0 : item.num}}</span>
+          <button type="button" @click="add(item,index)">+</button>
+        </li>
       </ul>
     </div>
     <div class="right">计算部分</div>
@@ -20,6 +22,28 @@ export default {
     return {
       lists: this.$store.state.lists
     };
+  },
+  methods: {
+    minus(item, index) {
+      if (typeof item.num === "undefined") {
+        item.num = 0;
+      }
+      item.num--;
+      if (item.num < 0) {
+        item.num = 0;
+      }
+      this.$set(this.lists,index,item);
+    },
+    add(item, index) {
+      if (typeof item.num === "undefined") {
+        item.num = 0;
+      }
+      item.num++;
+      if (item.num > 100) {
+        item.num = 100;
+      }
+      this.$set(this.lists,index,item);
+    }
   }
 };
 </script>

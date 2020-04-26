8dc7207b1ea6fc5292c62dd7a0772b3689dca9f5
guojin <jerrychane@126.com>
2020-04-26 22:00:34 +0800

    2020.04.26 SUN 17WEEK Beijing Koa MiddleWare

--- learn/resource/index.js
+++ learn/resource/index.js
@@ -3,14 +3,21 @@ const Router = require('koa-router')
 const app = new Koa();
 const router = new Router();
 
-router.get('/')
-
-// 1.request,method,respond
-// 2.api url => function,router?
-// 3.ctx,async
-app.use(async ctx =>{
+router.get('/', ctx=>{
     console.log(ctx);
     console.log(ctx.request);
     ctx.body = "Hello World!!";
 })
+
+router.get('/api', ctx=>{
+    console.log(ctx);
+    console.log(ctx.request);
+    ctx.body = "Hello Api!!";
+})
+
+// 1.request,method,respond
+// 2.api url => function,router?
+// 3.ctx,async
+app.use(router.routes())
+   .use(router.allowedMethods())
 app.listen(3000);
---
+++ learn/resource/middleware.js
@@ -0,0 +1,29 @@
+const Koa = require('koa');
+// const Router = require('koa-router');
+const app = new Koa();
+// const router = new Router();
+const middleware = function async(ctx,next) {
+    console.log('this is middleware!');
+    console.log(ctx.request.path);
+    // next();
+}
+
+const middleware1 = function async(ctx,next) {
+    console.log('this is middleware1!');
+    console.log(ctx.request.path);
+    next();
+    console.log('this is middleware1! ending');
+}
+
+const middleware2 = function async(ctx,next) {
+    console.log('this is middleware2!');
+    console.log(ctx.request.path);
+    next();
+    console.log('this is middleware2! ending');
+}
+
+app.use(middleware1);
+app.use(middleware2);
+app.use(middleware);
+
+app.listen(3000);

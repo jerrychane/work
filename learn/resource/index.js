dc92e6330b12b12974771f4c2fe6139ba9efdbfa
guojin <jerrychane@126.com>
2020-04-26 21:37:25 +0800

    2020.04.26 SUN 17WEEK Beijing Koa Router

--- learn/resource/index.js
+++ learn/resource/index.js
@@ -1,8 +1,16 @@
 const Koa = require('koa');
+const Router = require('koa-router')
 const app = new Koa();
+const router = new Router();
 
+router.get('/')
+
+// 1.request,method,respond
+// 2.api url => function,router?
+// 3.ctx,async
 app.use(async ctx =>{
-    ctx.body = "Hello World!!!";
+    console.log(ctx);
+    console.log(ctx.request);
+    ctx.body = "Hello World!!";
 })
-
 app.listen(3000);

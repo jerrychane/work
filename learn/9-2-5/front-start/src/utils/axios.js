2a3c491e23e298ccb3a50e0aaf4749a87c77d13b
guojin <jerrychane@126.com>
2020-06-06 21:06:09 +0800

    2020.06.06  SAT 23WEEK Beijing 2-6 axios封装进阶-业务示例、逻辑代码、配置

--- learn/9-2-5/front-start/src/utils/axios.js
+++ learn/9-2-5/front-start/src/utils/axios.js
@@ -1,9 +1,14 @@
 // 封装 axios 的请求，返回重新封装的数据格式
 // 对错误的统一处理
 import axios from 'axios'
+import config from '@/config'
 import errorHandle from './errorHandle'
 
-const instance = axios.create()
+const instance = axios.create({
+    baseURL: process.env.NODE_ENV === "development" ? config.baseURL.dev : config.baseURL.pro,
+    headers: { 'Content-Type': 'application/json;chartset=uft-8' },
+    timeout: 10000
+})
 // Add a request interceptor 请求拦截器
 instance.interceptors.request.use((config) => {
     // Do something before request is sent

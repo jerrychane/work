646b8aa0fe3be3302d91a4901d31c1ba9bf093da
jerrychane <jerrychane@126.com>
2020-06-27 23:32:04 +0800

    2020.06.27 SAT 26WEEK Beijing 1-9 完成消息&其他设置页面静态样式(CSS基础)

--- learn/10-4-1/front-start/src/components/user/Other.vue
+++ learn/10-4-1/front-start/src/components/user/Other.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>user others</div>
+  <div class="fly-panel fly-panel-user flex" pad20>正在开发中...</div>
 </template>
 
 <script>
@@ -9,4 +9,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.flex {
+  display: flex;
+  font-size: 18px;
+  color: #333;
+  justify-content: center;
+  align-items: center;
+}
 </style>

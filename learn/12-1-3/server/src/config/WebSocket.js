547b2985c58a6a1182a7a06a8fde0f49a754f07a
jerrychane <jerrychane@126.com>
2020-07-22 23:13:36 +0800

    2020.07.22 WED  30WEEK Beijing 4-4 实时消息开发-后端WebSocket封装

---
+++ learn/12-1-3/server/src/config/WebSocket.js
@@ -0,0 +1,39 @@
+import WebSocket from 'ws'
+
+class WebSocketServer {
+  constructor(config = {}) {
+    const defaultConfig = {
+      port: 3001,
+      timeInterval: 30 * 1000,
+      isAuth: true
+    }
+    // 最终配置
+    const finalConfig = { ...defaultConfig, ...config }
+    this.wss = {}
+    this.interval = finalConfig.timeInterval
+    this.isAuth = finalConfig.isAuth
+    this.port = finalConfig.port
+    this.options = config.options || {}
+  }
+
+  // 初始化 websocket 服务
+  init () {
+    this.wss = new WebSocket.Server({ port: this.port, ...this.options })
+    // 连接信息
+    this.wss.on('connection', (ws) => {
+      ws.isAlive = true
+      ws.on('message', (msg) => {
+        this.onMessage(ws, msg)
+      })
+      ws.on('close', () => {
+        this.onClose(ws)
+      })
+    })
+  }
+  onMessage (ws, msg) {
+
+  }
+  onClose (ws) {
+
+  }
+}

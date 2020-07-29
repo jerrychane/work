cbc50471e7af46d9f7a2efa4087abd3971414376
jerrychane <jerrychane@126.com>
2020-07-29 22:39:39 +0800

    2020.07.29 WED 31WEEK Beijing  3-2 工程目录&自动生成路由

---
+++ learn/nuxt-demo/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt-link class="link" to="/create">create link</nuxt-link>
+    <nuxt-link class="link" to="/about">about link</nuxt-link>
+    <nuxt-link class="link" to="/list">list link</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.link {
+  margin-right: 10px;
+}
+</style>
---
+++ learn/nuxt-demo/pages/list.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    list page
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>

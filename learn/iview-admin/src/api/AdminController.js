d33a6b2f6227f19c3d3986b992c8b0084fefd523
jerrychane <jerrychane@126.com>
2020-10-10 07:15:56 +0800

    2020.10.10 SAT 41WEEK Beijing 2-1 首页顶部统计信息-新增用户统计

--- learn/iview-admin/src/api/AdminController.js
+++ learn/iview-admin/src/api/AdminController.js
@@ -1,8 +1,23 @@
+import { moment } from 'dayjs'
 class AdminController {
-  async getMenu (ctx) {}
-  async addMenu (ctx) {}
-  async updateMenu (ctx) {}
-  async deleteMenu (ctx) {}
+  async getMenu (ctx) { }
+  async addMenu (ctx) { }
+  async updateMenu (ctx) { }
+  async deleteMenu (ctx) { }
+  async getStatus (ctx) {
+    // const result = {}
+    // 1.顶部的 card
+    const inforCardData = []
+    const userNewCount = await User.find({ created: { $gte: moment().format('YYYY-MM-DD 00:00:00') } }).countDocuments()
+    inforCardData.push(userNewCount)
+    // 2.左侧的饼图数据
+    // 3.本周的右侧统计数据
+    // 4.底部的数据
+    ctx.body = {
+      code: 200,
+      data: ''
+    }
+  }
 }
 
 export default new AdminController()

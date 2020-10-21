848f4ba1f6fe45a7095dcc3e4bbd71d33e6fe787
jerrychane <jerrychane@126.com>
2020-10-21 22:13:44 +0800

    Mint 项目初始化

--- learn/webapp/babel.config.js
+++ learn/webapp/babel.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset',
-    ["es2015", { "modules": false }]
-  ],
-  plugins:
-  [["component", 
-    {
-      "libraryName": "mint-ui",
-      "style": true
-    }
-  ]],
+  presets: [['@vue/cli-plugin-babel/preset', { modules: false }]],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'mint-ui',
+        style: true
+      }
+    ]
+  ]
 }

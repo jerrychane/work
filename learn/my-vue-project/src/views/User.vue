c58a2cd7d9ab8ce4b9a649ccc264aa154abef5dd
jerrychane <jerrychane@126.com>
2020-12-11 22:43:30 +0800

    ES6-11：获取用户信息列表

---
+++ learn/my-vue-project/src/views/User.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <div>用户信息</div>
+  <ul>
+    <li v-for="(item,index) in userList" :key="index">{{item.name}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      userList: []
+    }
+  },
+  created () {
+    axios.get('http://jsonplaceholder.typicode.com/users').then(res => {
+      console.log(res)
+      this.userList = res.data
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<style>
+li {
+  list-style: none;
+}
+</style>

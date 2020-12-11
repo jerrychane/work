181a245fa9bc1fc3649c93318500c9fe73078eaa
jerrychane <jerrychane@126.com>
2020-12-11 22:09:32 +0800

    ES6-11：获取用户信息列表

--- learn/my-vue-project/src/router/index.js
+++ learn/my-vue-project/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import User from '../views/User.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: User
   },
   {
     path: '/about',

7b4bf8e7bad2f0454d8e29fcb92c22a48e23b91b
jerrychane <jerrychane@126.com>
2020-12-20 21:28:15 +0800

    2020年12月20日 周日 第51周  ES6-11:组件按需加载

--- learn/my-vue-project/src/router/index.js
+++ learn/my-vue-project/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
 import User from '../views/User.vue'
-import Upload from '../views/Upload.vue'
 
 Vue.use(VueRouter)
 
@@ -14,15 +12,7 @@ const routes = [
   }, {
     path: '/upload',
     name: 'Upload',
-    component: Upload
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/Upload.vue')
   }
 ]
 

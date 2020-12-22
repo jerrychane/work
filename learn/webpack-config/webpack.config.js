116afe8b776423cdfa1310159f5e61a1f9768721
jerrychane <jerrychane@126.com>
2020-12-22 23:27:12 +0800

    2020年12月22日 周二 第52周 ES6-11：Wepback 配置

--- learn/webpack-config/webpack.config.js
+++ learn/webpack-config/webpack.config.js
@@ -1,4 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
 module.exports = {
   mode: 'development',
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "static", to: "static" },
+      ],
+    }),
+  ]
 }
